1e65f131f0382644b1e23e7d837b3d710d3cd18e
David Carley <dacarley@gmail.com>
2017-11-23 22:27:06 -0800

    Trying another method for installing yarn.

--- ci/setup.sh
+++ ci/setup.sh
@@ -3,7 +3,11 @@
 source ./ci/common.sh
 
 log "Installing global dependencies..."
-npm install -g yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get update
+apt-get install yarn
+elapsed
 
 log "Running 'yarn'..."
 yarn config set cache-folder /builds/BlitzPick/bp-contracts/.yarn
