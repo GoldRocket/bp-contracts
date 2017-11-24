d0c32431f2a79ffc0f950f01e2aad376f75f46a3
David Carley <dacarley@gmail.com>
2017-11-23 22:32:09 -0800

    Removing yarn install step, on the theory that yarn is already installed in Node 6.10 and later docker images.

--- ci/setup.sh
+++ ci/setup.sh
@@ -2,13 +2,6 @@
 
 source ./ci/common.sh
 
-log "Installing global dependencies..."
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-apt-get update
-apt-get install yarn
-elapsed
-
 log "Running 'yarn'..."
 yarn config set cache-folder /builds/dacarley/bp-contracts/.yarn
 yarn
