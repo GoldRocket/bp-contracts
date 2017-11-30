5a5d8fbaf695833fd0db8bd5e94346408b9f500e
David Carley <dacarley@gmail.com>
2017-11-30 11:05:51 -0800

    Fix yarn cache path for CI.

--- ci/setup.sh
+++ ci/setup.sh
@@ -3,6 +3,6 @@
 source ./ci/common.sh
 
 log "Running 'yarn'..."
-yarn config set cache-folder /builds/dacarley/bp-contracts/.yarn
+yarn config set cache-folder /builds/BlitzPick/bp-contracts/.yarn
 yarn
 elapsed
