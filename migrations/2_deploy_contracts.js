b50585c2e588779f8751acc7e39977e470688f71
David Carley <david@blitzpick.co>
2018-01-17 11:13:51 -0800

    Getting ready for final release.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,9 +3,11 @@ const moment = require("moment");
 const SafeMath = artifacts.require("libraries/SafeMath");
 const BPZSmartTokenSale = artifacts.require("BPZSmartTokenSale");
 
+const tokenSaleStart = moment("2018-01-22T08:00:00.000-08:00").unix();
+
 module.exports = (deployer) => {
     deployer.deploy(SafeMath);
 
     deployer.link(SafeMath, BPZSmartTokenSale);
-    deployer.deploy(BPZSmartTokenSale, moment().add(1, "day").unix());
+    deployer.deploy(BPZSmartTokenSale, tokenSaleStart);
 };
--- truffle.js
+++ truffle.js
@@ -1,8 +1,33 @@
 require("babel-register");
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const mnemonicKey = "BLITZPREDICT_TOKENSALE_MNEMONIC";
+const accessTokenKey = "INFURA_ACCESS_TOKEN";
+
+const gasLimit = 4 * 1000 * 1000; // The BPZSmartTokenSale contract requires aprox 3.7 mil
+const gwei = 1000 * 1000 * 1000;
+const gasPrice = 10 * gwei;
 
 module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
     // to customize your Truffle configuration!
+
+    networks: {
+        live: {
+            provider: () => createInfuraProvider("mainnet"),
+            network_id: 1,
+            gas: gasLimit,
+            gasPrice
+        },
+
+        ropsten: {
+            provider: () => createInfuraProvider("ropsten"),
+            network_id: 3,
+            gas: gasLimit,
+            gasPrice
+        }
+    },
+
     solc: {
         optimizer: {
             enabled: true,
@@ -10,3 +35,16 @@ module.exports = {
         }
     }
 };
+
+function createInfuraProvider(network) {
+    const mnemonic = process.env[mnemonicKey];
+    const accessToken = process.env[accessTokenKey];
+
+    if (!mnemonic || !accessToken) {
+        // eslint-disable-next-line no-console
+        console.error(`Env vars ${mnemonicKey} and ${accessTokenKey} must be set!`);
+        process.exit();
+    }
+
+    return new HDWalletProvider(mnemonic, `https://${network}.infura.io/${accessToken}`);
+}
